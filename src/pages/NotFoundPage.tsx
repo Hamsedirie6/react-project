e702a45ae808ca37da454add3fcada5f11e6c2fd
hamsedirie6 <hamse_03@hotmail.se>
2025-10-03 17:48:11 +0200

    Uppdaterat så att den anpassas till min application

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,17 +1,18 @@
 import { Link, useLocation } from "react-router-dom";
 
-NotFoundPage.route = {
-  path: '*'
-};
-
 export default function NotFoundPage() {
-  return <>
-    <h2>Not Found: 404</h2>
-    <p>
-      We are sorry, but there doesn't seem to be any page on this
-      site that matches the url:
-    </p>
-    <p><strong>{useLocation().pathname.slice(1)}</strong></p>
-    <p>Please <Link to="/">visit the start page</Link> instead.</p>
-  </>;
+  const path = useLocation().pathname;
+
+  return (
+    <>
+      <h2>Sidan kunde inte hittas (404)</h2>
+      <p>
+        Tyvärr finns det ingen sida som matchar adressen:
+      </p>
+      <p><strong>{path}</strong></p>
+      <p>
+        Gå tillbaka till <Link to="/">startsidan</Link>.
+      </p>
+    </>
+  );
 }
